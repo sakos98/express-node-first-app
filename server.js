39cc91b72eb55af49d8184f46eab32bbe7e308c5
Michal Sak <michusakowicz@gmail.com>
2023-08-26 14:33:21 +0200

    Add first layout on main.hbs & dark.hbs

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
+
 const app = express();
+
+app.engine('hbs', hbs());
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
     res.sendFile(path.join(__dirname, `/views/${name}`));
   };
   next();
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use('/user', (req, res, next) => {
@@ -17,36 +27,40 @@ app.use('/user', (req, res, next) => {
   next();
 });
 
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello ${req.params.name}`);
+});
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' })
 });
 
 app.get('user/settings', (req, res) => {
-  res.show('userSettings.html');
+  res.render('userSettings')
 });
 
 app.get('user/panel', (req, res) => {
-  res.show('userPanel.html');
+  res.render('userPanel')
 });
 
-// app.get('/contact', (req, res) => {
-//   res.show('contact.html');
-// });
+app.get('/contact', (req, res) => {
+  res.render('contact')
+});
 
-// app.get('/info', (req, res) => {
-//   res.show('info.html');
-// });
+app.get('/info', (req, res) => {
+  res.render('info')
+});
 
-// app.get('/history', (req, res) => {
-//   res.show('history.html');
-// });
+app.get('/history', (req, res) => {
+  res.render('history')
+});
 
 
 app.use((req, res) => {
