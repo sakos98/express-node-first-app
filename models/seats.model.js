7c7ec0134e015324f88253e08968ecc02d16dafa
Michal Sak <michusakowicz@gmail.com>
2023-09-29 14:42:50 +0200

    mongo in practise work

--- models/seats.model.js
+++ models/seats.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const seatSchema = new mongoose.Schema({
   day: { type: Number, require: true },
   seat: { type: Number, require: true },
-  clien: { type: String, required: true },
+  client: { type: String, required: true },
   email: { type: String, required: true },
 });
 
